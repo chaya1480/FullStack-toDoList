782a12f2ccc18cf0d3f19d755b059e120d0df925
chaya1480 <chaya86486@gmail.com>
2025-02-21 00:02:34 +0200

    Update API_URL

--- ToDo-React/src/api.js
+++ ToDo-React/src/api.js
@@ -12,5 +12,6 @@ const apiClient = axios.create({
     baseURL: process.env.REACT_APP_API_URL || "http://localhost:5280",  
     headers: { 'Content-Type': 'application/json' }
 });
+console.log("API URL:", process.env.REACT_APP_API_URL);
 
 export default apiClient;
